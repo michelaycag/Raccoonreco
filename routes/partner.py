b805dacfc65bf9c3fd2218fe8bacd97897980c83
Christian <chuelmo@gmail.com>
2022-10-17 03:57:37 -0300

    Endpoint logout created

--- routes/partner.py
+++ routes/partner.py
@@ -6,7 +6,7 @@ from flask_jwt_extended import jwt_required
 
 
 @routes.route("/partner", methods=['POST'])
-@jwt_required()
+@jwt_required(fresh=True)
 def insertPartner():
     name = request.json.get("name", None)
     partnerId = int(request.json.get("partnerId", None))
@@ -25,34 +25,35 @@ def insertPartner():
 
     try:
         cur = con.cursor()
-        cur.execute("INSERT INTO partners (name, partnerId, document,authorized , contactNumber) VALUES (%s,%s,%s,%s,%s)", (name, partnerId, document,authorized, contactNumber))
-        cur.execute("SELECT * from partners p WHERE p.partnerId = %s", [partnerId])
+        cur.execute("INSERT INTO partners (name, partnerId, document,authorized , contactNumber) VALUES (%s,%s,%s,%s,%s)",
+                    (name, partnerId, document, authorized, contactNumber))
+        cur.execute(
+            "SELECT * from partners p WHERE p.partnerId = %s", [partnerId])
         user = cur.fetchone()
         con.commit()
         cur.close()
         return jsonify({"msg": "User inserted successfully", "user": user}), 201
-    except psycopg2.DatabaseError as e :
+    except psycopg2.DatabaseError as e:
         return jsonify({"msg": "Something went wrong! Please try again later", "error": e}), 500
-   
 
 
 @routes.route("/partners", methods=['GET'])
-@jwt_required()
+@jwt_required(fresh=False)
 def getAllPartners():
     try:
-        cur = con.cursor()    
+        cur = con.cursor()
         cur.execute("SELECT * FROM partners")
         users = cur.fetchall()
         con.commit()
         cur.close()
         return jsonify({"data": users}), 200
-    except psycopg2.DatabaseError as e :
+    except psycopg2.DatabaseError as e:
         return jsonify({"msg": "Something went wrong! Please try again later", "error": e}), 500
 
 
 @routes.route("/partner", methods=['PATCH'])
 @jwt_required()
-def updatePartner():
+def updatePartner(fresh=True):
     name = request.json.get("name", None)
     partnerId = int(request.json.get("partnerId", None))
     document = request.json.get("document", None)
@@ -67,21 +68,22 @@ def updatePartner():
     if contactNumber is None:
         return jsonify({"msg": "All fields are required!"}), 400
 
-
     try:
         cur = con.cursor()
-        cur.execute("UPDATE partners SET name= %s, document= %s, contactNumber= %s WHERE partnerId= %s", (name, document, contactNumber, partnerId))
-        cur.execute("SELECT * from partners p WHERE p.partnerId = %s", [partnerId])
+        cur.execute("UPDATE partners SET name= %s, document= %s, contactNumber= %s WHERE partnerId= %s",
+                    (name, document, contactNumber, partnerId))
+        cur.execute(
+            "SELECT * from partners p WHERE p.partnerId = %s", [partnerId])
         user = cur.fetchone()
         con.commit()
         cur.close()
         return jsonify({"msg": "User updated successfully", "user": user}), 200
-    except psycopg2.DatabaseError as e :
+    except psycopg2.DatabaseError as e:
         return jsonify({"msg": "Something went wrong! Please try again later", "error": e}), 500
-   
+
 
 @routes.route("/partner", methods=['DELETE'])
-@jwt_required()
+@jwt_required(fresh=True)
 def deletePartner():
     partnerId = int(request.json.get("partnerId", None))
 
@@ -90,14 +92,15 @@ def deletePartner():
 
     try:
         cur = con.cursor()
-        cur.execute("DELETE FROM partners p WHERE p.partnerid=%s", [partnerId] )
+        cur.execute("DELETE FROM partners p WHERE p.partnerid=%s", [partnerId])
         rowsDeleted = cur.rowcount
-        cur.execute("SELECT * from partners p WHERE p.partnerId = %s", [partnerId])
+        cur.execute(
+            "SELECT * from partners p WHERE p.partnerId = %s", [partnerId])
         con.commit()
         cur.close()
         if (rowsDeleted != 1):
             return jsonify({"msg": str(rowsDeleted) + " rows deleted"}), 200
         else:
             return jsonify({"msg": str(rowsDeleted) + " row deleted"}), 200
-    except psycopg2.DatabaseError as e :
+    except psycopg2.DatabaseError as e:
         return jsonify({"msg": "Something went wrong! Please try again later", "error": e}), 500
